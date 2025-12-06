7f772e8103e498aa1397676a2ddede336436501e
akshit1662002 <tyagiakshit100@gmail.com>
2025-12-06 23:57:11 +0530

    complete 4-9

---
+++ 4-9-singleNumber.js
@@ -0,0 +1,15 @@
+var singleNumber = function (nums) {
+  const hash = {};
+  for (let i = 0; i < nums.length; i++) {
+    if (!hash[nums[i]]) {
+      hash[nums[i]] = 1;
+    } else {
+      hash[nums[i]]++;
+    }
+  }
+  for (let i = 0; i < nums.length; i++) {
+    if (hash[nums[i]]) {
+      return nums[i];
+    }
+  }
+};
